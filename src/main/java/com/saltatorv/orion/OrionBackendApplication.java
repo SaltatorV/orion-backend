f20e4b0e696f3adadc48ffa9a81038153b33f220
Saltator <saltatorv@gmail.com>
2026-05-18 22:30:57 +0200

    [1]:chore - initialize project

---
+++ src/main/java/com/saltatorv/orion/OrionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saltatorv.orion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saltatorv/orion/OrionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saltatorv.orion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
